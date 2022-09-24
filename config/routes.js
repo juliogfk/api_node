e801bd274eddcfff73589f56ca55e2a12f6ad576
juliogfk <juliogfk1@gmail.com>
2022-09-24 16:40:51 -0300

    criação do arquivo routes

---
+++ config/routes.js
@@ -0,0 +1,65 @@
+/*
+ESTE ARQUIVO FOI CRIADO, POIS O INDEX.JS ESTÁ MUITO 
+ATAREFADO E ISSO NÃO É BOM.
+*/
+
+const express = require('express')
+const routes = express.Router()
+
+/*
+DEPOIS DOS COMENTÁRIOS NO BLOCO DE NOTAS. COMEÇAR AQUI \/
+Banco de dados em tempo de execução. 
+Sempre que o nodemon modificar, ele será limpo. 
+
+*/
+let db = [
+    { '1': { Nome: 'Cliente 1', Idade: '20' } },
+    { '2': { Nome: 'Cliente 2', Idade: '20' } },
+    { '3': { Nome: 'Cliente 3', Idade: '20' } }
+]
+
+/*
+VAMOS USAR O HTTP(PROTOCOLO DE INTERNET)
+FAREMOS 4 OPERAÇÕES: INSERIR(POST), MODIFICAR(PUSH), 
+DELETAR(DELETE) E SELECIONAR(GET).
+INSTALA O POSTMAN E COLOCA A URL DO LOCALHOST LÁ E APERTA SEND.
+NO NAVEGADOR(COMO ESTAVA ANTES), NÃO SIMULA AS OPERAÇÕES.
+O POSTMAN, SIMULA! \/
+*/
+// Buscar Dados
+routes.get('/', (req, res) => {
+    return res.json(db)
+})
+
+/*
+O MÉTODO POST, PRECISA POR PARÂMETRO, ENVIAR UMA INFORMAÇÃO.
+POR ISSO O CONST(VARIÁVEL)
+*/
+// Inserir Dados
+routes.post('/add', (req, res) => {
+    const body = req.body
+
+    if(!body)
+        return res.status(400).end()
+
+    db.push(body)
+    return res.json(body)
+})
+/*
+VAI SER DELETADO PELO ID, PODE SE COLOCAR O NOME FICARIA: /:ID/:NAME
+*/
+routes.delete('/:id', (req, res)=> {
+    const id = req.params.id
+
+    let newDB = db.filter(item => {
+        if (!item[id])
+                return item
+    })
+
+    db = newDB
+
+    return res.send(newDB)
+})
+
+
+module.exports = routes
